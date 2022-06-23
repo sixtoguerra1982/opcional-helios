6d79b4c9666b51f01e0b0fca1293e009ff74271c
Felipe Guerra <sixto.guerra1982@gmail.com>
2022-06-22 21:07:52 -0400

    first commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get 'users/login'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get  "users/login"
-  get  "pages/about_us"
+  # get  "users/login"
+  get  "pages/test"
   root "pages#index"
+  get "pages/about_us"
 end
